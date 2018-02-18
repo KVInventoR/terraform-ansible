4425c53dc0c57f2d90f041f7155d93a961bd71b6
InventoR <e.ahmatgareev@gmail.com>
2018-02-19 01:13:53 +0200

    addded iam

---
+++ cloudtrail.tf
@@ -0,0 +1,45 @@
+# CLOUDTRAIL CONFIGURATION
+# =============================================================================
+resource "aws_cloudtrail" "cloudtrail-logging" {
+  name                          = "cloudtrail-logging"
+  s3_bucket_name                = "${aws_s3_bucket.cloudtrail-s3bucket.id}"
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = "${var.CLOUDTRAIL_INCLUDE_GLOBAL_SERVICE_EVENTS}"
+  is_multi_region_trail         = "${var.CLOUDTRAIL_IS_MULTI_REGION}"
+}
+
+resource "aws_s3_bucket" "cloudtrail-s3bucket" {
+  bucket        = "cloudtrail-${var.DEFAULT_ROUTE53_ZONE}"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::cloudtrail-${var.DEFAULT_ROUTE53_ZONE}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::cloudtrail-${var.DEFAULT_ROUTE53_ZONE}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
---
+++ load_balancer.tf
@@ -0,0 +1,51 @@
+# ALB CONFIGURATION
+# =============================================================================
+resource "aws_alb" "jenkins-elb" {
+  name                       = "jenkins-elb"
+  security_groups            = ["${aws_security_group.elb-securitygroup.id}"]
+  internal                   = false
+  enable_deletion_protection = false
+
+  subnets = [
+    "${aws_subnet.main-public-1.id}",
+    "${aws_subnet.main-public-2.id}",
+  ]
+}
+
+resource "aws_alb_target_group" "jenkins-elb-target" {
+  name     = "jenkins-elb-target"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.main.id}"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+    path                = "/login"
+  }
+}
+
+resource "aws_alb_listener" "jenkins-elb-listener" {
+  depends_on        = ["aws_iam_server_certificate.jenkins-certificate"]
+  load_balancer_arn = "${aws_alb.jenkins-elb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${aws_iam_server_certificate.jenkins-certificate.arn}"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.jenkins-elb-target.0.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_target_group_attachment" "jenkins-elb-group-attach" {
+  target_group_arn = "${aws_alb_target_group.jenkins-elb-target.arn}"
+  target_id        = "${aws_instance.ec2-jenkins.id}"
+  port             = 8080
+}
+
+# https://dev.classmethod.jp/cloud/aws/terraform-alb/
+
