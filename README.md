127e0e6d87b8a5d4d5cd79bb1f8e2685608cc814
InventoR <e.ahmatgareev@gmail.com>
2018-02-19 15:11:09 +0200

    modified output:

---
+++ README.md
@@ -0,0 +1,1 @@
+This role required to create private ssh keys in folder: ssh-keys and ssl-certs for elb ssl.
--- output.tf
+++ output.tf
@@ -2,16 +2,8 @@ output "name_servers" {
   value = "${aws_route53_zone.default_hosted_zone.name_servers}"
 }
 
-output "elb" {
-  value = "${aws_alb.jenkins-elb.dns_name}"
-}
-
 output "certificate" {
   value = "${aws_iam_server_certificate.jenkins-certificate.arn}"
-}
-
-output "route53-elb" {
-  value = "${aws_route53_record.jenkins-elb.name}"
 }
 
 output "private_ip" {
@@ -24,4 +16,12 @@ output "public_ip" {
 
 output "public_dns" {
   value = "${aws_instance.ec2-jenkins.public_dns}"
+}
+
+output "elb" {
+  value = "${aws_alb.jenkins-elb.dns_name}"
+}
+
+output "route53-elb" {
+  value = "${aws_route53_record.jenkins-elb.name}"
 }
