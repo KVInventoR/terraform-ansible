e63cb9f36e22930289431247450a7dc09c3655cf
Dmitriy Kagarlickij <dmitriy@kagarlickij.com>
2018-02-24 21:00:13 +0200

    output has been changed

--- output.tf
+++ output.tf
@@ -1,15 +1,7 @@
-output "jenkins-private_ip" {
-  value = "${aws_instance.ec2-jenkins.*.private_ip}"
-}
-
 output "jenkins-public_dns" {
   value = "${aws_instance.ec2-jenkins.*.public_dns}"
 }
 
-output "lb-dns_name" {
-  value = "${aws_alb.jenkins-lb.dns_name}"
-}
-
-output "jenkins-dns_name" {
-  value = "jenkins.${data.aws_route53_zone.selected.name}"
+output "jenkins-private_ip" {
+  value = "${aws_instance.ec2-jenkins.*.private_ip}"
 }
